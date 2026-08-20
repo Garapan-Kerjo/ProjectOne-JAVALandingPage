7d544dfe99ccb8433b816dcf6a4c2418c1ea0284
Fvcklyx <mariofelixfauzi@gmail.com>
2026-08-20 17:51:18 +0700

    Tim Penyusun non italic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { korpusArsip, daftarPustaka, daftarPuisiMahasiswi, daftarPuisiMahasiswa } from "./Assets/data/korpus-data.mjs";
+import { korpusArsip, daftarPustaka, daftarPuisiMahasiswi } from "./Assets/data/korpus-data.mjs";
 
 const cursor = document.querySelector(".cursor");
 
@@ -298,11 +298,6 @@ const puisiPanels = [
     title: "Daftar Puisi Mahasiswi",
     count: daftarPuisiMahasiswi.length,
     contentHTML: "<ul class=\"list-content\">" + daftarPuisiMahasiswi.map(buildPuisiItemHTML).join("") + "</ul>"
-  },
-  {
-    title: "Daftar Puisi Mahasiswa",
-    count: daftarPuisiMahasiswa.length,
-    contentHTML: "<ul class=\"list-content\">" + daftarPuisiMahasiswa.map(buildPuisiItemHTML).join("") + "</ul>"
   }
 ];
 
